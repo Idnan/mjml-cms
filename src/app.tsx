30be6839c807aceb7535f0b97200d5f75b35db9a
Adnan Ahmed <idnan.se@gmail.com>
2020-05-09 15:57:44 +0400

    show hide mjml and data editors

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,9 @@ import { DataEditor } from './components/data-editor';
 
 export function App() {
 
+    const [mjmlWindow, setMjmlWindow] = useState(true);
+    const [dataWindow, setDataWindow] = useState(true);
+
     const [editorWindow, setEditorWindow] = useState(true);
     const [previewWindow, setPreviewWindow] = useState(true);
 
@@ -41,14 +44,37 @@ export function App() {
 
     return (
         <Flex flexDir='row' flex={ 1 } bg='#252426'>
-            {/*<Flex flexDirection={ 'column' }>*/ }
-            {/*    <SideMenu selectedTemplateName={ setTemplate } />*/ }
-            {/*</Flex>*/ }
+            {/*<Flex flexDirection={ 'column' }>
+                <SideMenu selectedTemplateName={ setTemplate } />
+            </Flex>*/ }
             <Flex flex={ 1 } flexDirection={ 'column' } bg={ '#1e1e1e' } d={ editorWindow ? 'flex' : 'none' }>
-                <Flex flex={ 1 } flexDirection={ 'column' }>
+                <Flex flex={ 1 } flexDirection={ 'column' } d={ mjmlWindow ? 'flex' : 'none' }>
                     <FileEditor content={ get(templateData, 'mjml', '') } mjml={ setMjml } />
                 </Flex>
-                <Flex flex={ 1 } flexDirection={ 'column' }>
+                <PseudoBox
+                    _hover={ { backgroundColor: '#535a63' } }
+                    onClick={ _ => setMjmlWindow(!mjmlWindow) }
+                    h={ '14px' }
+                    cursor={ 'pointer' }
+                    bg={ '#42474e' }
+                    borderBottom={ '1px solid #343436' }
+                    justifyContent='center'
+                    alignItems='center'
+                    d={ dataWindow ? 'flex' : 'none' }>
+                    <Icon name={ mjmlWindow ? 'chevron-up' : 'chevron-down' } />
+                </PseudoBox>
+                <PseudoBox
+                    _hover={ { backgroundColor: '#535a63' } }
+                    onClick={ _ => setDataWindow(!dataWindow) }
+                    h={ '14px' }
+                    cursor={ 'pointer' }
+                    bg={ '#42474e' }
+                    justifyContent='center'
+                    alignItems='center'
+                    d={ mjmlWindow ? 'flex' : 'none' }>
+                    <Icon name={ dataWindow ? 'chevron-down' : 'chevron-up' } />
+                </PseudoBox>
+                <Flex flex={ 1 } flexDirection={ 'column' } d={ dataWindow ? 'flex' : 'none' }>
                     <DataEditor data={ setData } />
                 </Flex>
             </Flex>
@@ -57,27 +83,27 @@ export function App() {
                 onClick={ () => {
                     setEditorWindow(!editorWindow);
                 } }
-                w={ '15px' }
+                w={ '14px' }
                 cursor={ 'pointer' }
                 bg={ '#42474e' }
                 borderRight={ '1px solid #343436' }
                 justifyContent='center'
                 alignItems='center'
                 d={ previewWindow ? 'flex' : 'none' }>
-                <Icon name={editorWindow ? 'chevron-left' : 'chevron-right'} />
+                <Icon name={ editorWindow ? 'chevron-left' : 'chevron-right' } />
             </PseudoBox>
             <PseudoBox
                 _hover={ { backgroundColor: '#535a63' } }
                 onClick={ () => {
                     setPreviewWindow(!previewWindow);
                 } }
-                w={ '15px' }
+                w={ '14px' }
                 cursor={ 'pointer' }
                 bg={ '#42474e' }
                 justifyContent='center'
                 alignItems='center'
                 d={ editorWindow ? 'flex' : 'none' }>
-                <Icon name={previewWindow ? 'chevron-right' : 'chevron-left'} />
+                <Icon name={ previewWindow ? 'chevron-right' : 'chevron-left' } />
             </PseudoBox>
             <Flex flex={ 1 } d={ previewWindow ? 'flex' : 'none' }>
                 <Box d={ 'flex' } flexGrow={ 1 } className='preview'>
