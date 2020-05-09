afae674fa9743d524421e87bee102c9abc55f65b
Adnan Ahmed <idnan.se@gmail.com>
2020-05-09 15:46:19 +0400

    option to collapse preivew and editor windows

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,7 @@
 import './app.css';
 import React, { useEffect, useState } from 'react';
-import { Box, Flex } from '@chakra-ui/core/dist';
+import { Box, Flex, Icon, PseudoBox } from '@chakra-ui/core/dist';
 import get from 'lodash/get';
-import SideMenu from './components/side-menu';
 import FileEditor from './components/file-editor';
 import { getTemplateService } from './services/get-template';
 import { Preview } from './components/preview';
@@ -10,6 +9,9 @@ import { renderTemplateService } from './services/render-template';
 import { DataEditor } from './components/data-editor';
 
 export function App() {
+
+    const [editorWindow, setEditorWindow] = useState(true);
+    const [previewWindow, setPreviewWindow] = useState(true);
 
     const [mjml, setMjml] = useState('');
     const [data, setData] = useState({});
@@ -25,7 +27,7 @@ export function App() {
         getTemplateService(template)
             .then(data => {
                 setMjml(data?.mjml || '');
-                setTemplateData(data)
+                setTemplateData(data);
             });
     }, [template]);
 
@@ -39,18 +41,45 @@ export function App() {
 
     return (
         <Flex flexDir='row' flex={ 1 } bg='#252426'>
-            <Flex flexDirection={ 'column' }>
-                <SideMenu selectedTemplateName={ setTemplate } />
-            </Flex>
-            <Flex flex={ 1 } flexDirection={ 'column' } bg={ '#1e1e1e' }>
-                <Flex flex={1} flexDirection={'column'}>
+            {/*<Flex flexDirection={ 'column' }>*/ }
+            {/*    <SideMenu selectedTemplateName={ setTemplate } />*/ }
+            {/*</Flex>*/ }
+            <Flex flex={ 1 } flexDirection={ 'column' } bg={ '#1e1e1e' } d={ editorWindow ? 'flex' : 'none' }>
+                <Flex flex={ 1 } flexDirection={ 'column' }>
                     <FileEditor content={ get(templateData, 'mjml', '') } mjml={ setMjml } />
                 </Flex>
-                <Flex flex={1} flexDirection={'column'}>
+                <Flex flex={ 1 } flexDirection={ 'column' }>
                     <DataEditor data={ setData } />
                 </Flex>
             </Flex>
-            <Flex flex={ 1 }>
+            <PseudoBox
+                _hover={ { backgroundColor: '#535a63' } }
+                onClick={ () => {
+                    setEditorWindow(!editorWindow);
+                } }
+                w={ '15px' }
+                cursor={ 'pointer' }
+                bg={ '#42474e' }
+                borderRight={ '1px solid #343436' }
+                justifyContent='center'
+                alignItems='center'
+                d={ previewWindow ? 'flex' : 'none' }>
+                <Icon name={editorWindow ? 'chevron-left' : 'chevron-right'} />
+            </PseudoBox>
+            <PseudoBox
+                _hover={ { backgroundColor: '#535a63' } }
+                onClick={ () => {
+                    setPreviewWindow(!previewWindow);
+                } }
+                w={ '15px' }
+                cursor={ 'pointer' }
+                bg={ '#42474e' }
+                justifyContent='center'
+                alignItems='center'
+                d={ editorWindow ? 'flex' : 'none' }>
+                <Icon name={previewWindow ? 'chevron-right' : 'chevron-left'} />
+            </PseudoBox>
+            <Flex flex={ 1 } d={ previewWindow ? 'flex' : 'none' }>
                 <Box d={ 'flex' } flexGrow={ 1 } className='preview'>
                     <Preview value={ get(templateData, 'html', '') } />
                 </Box>
