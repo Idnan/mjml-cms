62452ae52dc98562ec02b357536fa8878e27f9f9
Adnan Ahmed <idnan.se@gmail.com>
2020-05-09 16:14:54 +0400

    incresed gutter height width on show hide

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
-import './app.css';
-import React, { useEffect, useState } from 'react';
-import { Box, Flex, Icon, PseudoBox } from '@chakra-ui/core/dist';
 import get from 'lodash/get';
-import FileEditor from './components/file-editor';
-import { getTemplateService } from './services/get-template';
+import { Box, Flex, Icon, PseudoBox } from '@chakra-ui/core/dist';
+
+import './app.css';
 import { Preview } from './components/preview';
-import { renderTemplateService } from './services/render-template';
+import FileEditor from './components/file-editor';
+import React, { useEffect, useState } from 'react';
 import { DataEditor } from './components/data-editor';
+import { renderTemplateService } from './services/render-template';
 
 export function App() {
 
@@ -19,20 +19,20 @@ export function App() {
     const [mjml, setMjml] = useState('');
     const [data, setData] = useState({});
 
-    const [template, setTemplate] = useState('');
+    // const [template, setTemplate] = useState('');
     const [templateData, setTemplateData] = useState({});
 
-    useEffect(() => {
-        if (!template) {
-            return;
-        }
-
-        getTemplateService(template)
-            .then(data => {
-                setMjml(data?.mjml || '');
-                setTemplateData(data);
-            });
-    }, [template]);
+    // useEffect(() => {
+    //     if (!template) {
+    //         return;
+    //     }
+    //
+    //     getTemplateService(template)
+    //         .then(data => {
+    //             setMjml(data?.mjml || '');
+    //             setTemplateData(data);
+    //         });
+    // }, [template]);
 
     useEffect(() => {
         if (!mjml) {
@@ -47,66 +47,62 @@ export function App() {
             {/*<Flex flexDirection={ 'column' }>
                 <SideMenu selectedTemplateName={ setTemplate } />
             </Flex>*/ }
-            <Flex flex={ 1 } flexDirection={ 'column' } bg={ '#1e1e1e' } d={ editorWindow ? 'flex' : 'none' }>
-                <Flex flex={ 1 } flexDirection={ 'column' } d={ mjmlWindow ? 'flex' : 'none' }>
+            <Flex flex={ 1 } flexDirection='column' bg='#1e1e1e' d={ editorWindow ? 'flex' : 'none' }>
+                <Flex flex={ 1 } flexDirection='column' d={ mjmlWindow ? 'flex' : 'none' }>
                     <FileEditor content={ get(templateData, 'mjml', '') } mjml={ setMjml } />
                 </Flex>
                 <PseudoBox
                     _hover={ { backgroundColor: '#535a63' } }
                     onClick={ _ => setMjmlWindow(!mjmlWindow) }
-                    h={ '14px' }
-                    cursor={ 'pointer' }
-                    bg={ '#42474e' }
-                    borderBottom={ '1px solid #343436' }
+                    h={ mjmlWindow ? '14px' : '20px' }
+                    d={ dataWindow ? 'flex' : 'none' }
+                    cursor='pointer'
+                    bg='#42474e'
+                    borderBottom='1px solid #343436'
                     justifyContent='center'
-                    alignItems='center'
-                    d={ dataWindow ? 'flex' : 'none' }>
+                    alignItems='center'>
                     <Icon name={ mjmlWindow ? 'chevron-up' : 'chevron-down' } />
                 </PseudoBox>
                 <PseudoBox
                     _hover={ { backgroundColor: '#535a63' } }
                     onClick={ _ => setDataWindow(!dataWindow) }
-                    h={ '14px' }
-                    cursor={ 'pointer' }
-                    bg={ '#42474e' }
+                    h={ dataWindow ? '14px' : '20px' }
+                    d={ mjmlWindow ? 'flex' : 'none' }
+                    cursor='pointer'
+                    bg='#42474e'
                     justifyContent='center'
-                    alignItems='center'
-                    d={ mjmlWindow ? 'flex' : 'none' }>
+                    alignItems='center'>
                     <Icon name={ dataWindow ? 'chevron-down' : 'chevron-up' } />
                 </PseudoBox>
-                <Flex flex={ 1 } flexDirection={ 'column' } d={ dataWindow ? 'flex' : 'none' }>
+                <Flex flex={ 1 } flexDirection='column' d={ dataWindow ? 'flex' : 'none' }>
                     <DataEditor data={ setData } />
                 </Flex>
             </Flex>
             <PseudoBox
                 _hover={ { backgroundColor: '#535a63' } }
-                onClick={ () => {
-                    setEditorWindow(!editorWindow);
-                } }
-                w={ '14px' }
-                cursor={ 'pointer' }
-                bg={ '#42474e' }
-                borderRight={ '1px solid #343436' }
+                onClick={ _ => setEditorWindow(!editorWindow) }
+                w={ editorWindow ? '14px' : '20px' }
+                d={ previewWindow ? 'flex' : 'none' }
+                cursor='pointer'
+                bg='#42474e'
+                borderRight='1px solid #343436'
                 justifyContent='center'
-                alignItems='center'
-                d={ previewWindow ? 'flex' : 'none' }>
+                alignItems='center'>
                 <Icon name={ editorWindow ? 'chevron-left' : 'chevron-right' } />
             </PseudoBox>
             <PseudoBox
                 _hover={ { backgroundColor: '#535a63' } }
-                onClick={ () => {
-                    setPreviewWindow(!previewWindow);
-                } }
-                w={ '14px' }
-                cursor={ 'pointer' }
-                bg={ '#42474e' }
+                onClick={ _ => setPreviewWindow(!previewWindow) }
+                w={ previewWindow ? '14px' : '20px' }
+                d={ editorWindow ? 'flex' : 'none' }
+                cursor='pointer'
+                bg='#42474e'
                 justifyContent='center'
-                alignItems='center'
-                d={ editorWindow ? 'flex' : 'none' }>
+                alignItems='center'>
                 <Icon name={ previewWindow ? 'chevron-right' : 'chevron-left' } />
             </PseudoBox>
             <Flex flex={ 1 } d={ previewWindow ? 'flex' : 'none' }>
-                <Box d={ 'flex' } flexGrow={ 1 } className='preview'>
+                <Box d='flex' flexGrow={ 1 } className='preview'>
                     <Preview value={ get(templateData, 'html', '') } />
                 </Box>
             </Flex>
